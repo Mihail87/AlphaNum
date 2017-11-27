0c29ddf01c06b2db788464fe9356353d281809bb
Doru Mihai Leonte <mihai.leonte@me.com>
2017-11-27 20:44:49 +0200

    Initial Commit

---
+++ Alpha Num/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alpha Num
+//
+//  Created by Mihai Leonte on 11/27/17.
+//  Copyright © 2017 Mihai Leonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
