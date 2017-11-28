9fcc807619ef65dabd6a10b752cd0e2cfc0819c3
Mihai Leonte <mihai.leonte@icloud.com>
2017-11-28 12:35:50 +0200

    final version + app icon

--- Alpha Num/ViewController.swift
+++ Alpha Num/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numResultLabel: UILabel!
     @IBOutlet weak var labelAlpha: UILabel!
     @IBOutlet weak var correctLabel: UILabel!
     @IBOutlet weak var incorrectLabel: UILabel!
@@ -25,9 +26,12 @@ class ViewController: UIViewController {
     var correctCounter: Int = 0
     var incorrectCounter: Int = 0
     var finishedAlphabet: Bool = false
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        numResultLabel.isHidden = true
         
         numericButton.isHidden = true
         numericButton.backgroundColor = .clear
@@ -58,11 +62,19 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-    @IBAction func answerCorrect() {
+    
+    func increaseCorrectCounter() {
         correctCounter += 1
         correctLabel.text = String(correctCounter)
-        
+    }
+    
+    func increaseIncorrectCounter() {
+        incorrectCounter += 1
+        incorrectLabel.text = String(incorrectCounter)
+    }
+
+    @IBAction func answerCorrect() {
+        increaseCorrectCounter()
         let newLetter = alphabetProvider.randomLetters(doRemove: true)
         
         if newLetter == "N/A" {
@@ -75,41 +87,86 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func answerWrong() {
-        incorrectCounter += 1
-        incorrectLabel.text = String(incorrectCounter)
-        
+        increaseIncorrectCounter()
         labelAlpha.text = alphabetProvider.randomLetters(doRemove: false)
+    }
+    
+    func newOperation() {
+        numProvider.newNumbersPair()
+        labelAlpha.text = numProvider.operationString
+        numericButton.setTitle(numProvider.labels[0], for: UIControlState.normal)
+        numeric2Button.setTitle(numProvider.labels[1], for: UIControlState.normal)
+        numeric3Button.setTitle(numProvider.labels[2], for: UIControlState.normal)
+    }
+    func showNum() {
+        labelAlpha.font = labelAlpha.font.withSize(80.0)
+        correctButton.isHidden = true
+        incorrectButton.isHidden = true
+        numericButton.isHidden = false
+        numeric2Button.isHidden = false
+        numeric3Button.isHidden = false
+    }
+    func showAlpha() {
+        numResultLabel.isHidden = true
+        numericButton.isHidden = true
+        numeric2Button.isHidden = true
+        numeric3Button.isHidden = true
+        correctButton.isHidden = false
+        incorrectButton.isHidden = false
+    }
+    @objc func hideNumResultLabel() {
+        numResultLabel.isHidden = true
+    }
+    func showNumResult(isCorrect correct: Bool) {
+        numResultLabel.isHidden = false
+        if correct {
+            numResultLabel.text = "🥇"
+        } else {
+            numResultLabel.text = "🤪"
+        }
+        if timer == nil {
+            Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(hideNumResultLabel), userInfo: nil, repeats: false)
+        } else {
+            timer?.fire()
+        }
+    }
+    func checkNumResult(forButton button: UIButton) {
+        if let buttonLabel = button.titleLabel?.text {
+            if Int(buttonLabel) == numProvider.firstNum + numProvider.secondNum {
+                increaseCorrectCounter()
+                showNumResult(isCorrect: true)
+                newOperation()
+            } else {
+                increaseIncorrectCounter()
+                showNumResult(isCorrect: false)
+                newOperation()
+            }
+        }
+    }
+    @IBAction func pressFirstButton() {
+        checkNumResult(forButton: numericButton)
+    }
+    @IBAction func pressSecondButton() {
+        checkNumResult(forButton: numeric2Button)
+    }
+    @IBAction func pressThirdButton() {
+        checkNumResult(forButton: numeric3Button)
     }
     
     @IBAction func selectIndex(_ sender: Any) {
         if segmentedControl.selectedSegmentIndex == 1 {
-            labelAlpha.font = labelAlpha.font.withSize(80.0)
-            correctButton.isHidden = true
-            incorrectButton.isHidden = true
-            
-            numProvider.newNumbersPair()
-            labelAlpha.text = numProvider.operationString
-            
-            numericButton.isHidden = false
-            numeric2Button.isHidden = false
-            numeric3Button.isHidden = false
-
-            print(numProvider.labels)
-            numericButton.titleLabel!.text = numProvider.labels[0]
-            numeric2Button.titleLabel!.text = numProvider.labels[1]
-            numeric3Button.titleLabel!.text = numProvider.labels[2]
-            
+            showNum()
+            newOperation()
         } else if segmentedControl.selectedSegmentIndex == 0 {
             labelAlpha.font = labelAlpha.font.withSize(125.0)
             if finishedAlphabet {
                 labelAlpha.text = "🏆"
-            } else {
+                numResultLabel.isHidden = true
                 numericButton.isHidden = true
                 numeric2Button.isHidden = true
                 numeric3Button.isHidden = true
-                correctButton.isHidden = false
-                incorrectButton.isHidden = false
-                
+            } else {
+                showAlpha()
                 labelAlpha.text = alphabetProvider.randomLetters(doRemove: false)
             }
         }
